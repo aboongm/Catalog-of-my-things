8cf8c58c37f7779a0bd1581fe27e0c92174d14e2
sainingo <nickmentor421@gmail.com>
2022-07-04 16:34:15 +0300

    Implement startup actions

--- app.rb
+++ app.rb
@@ -18,13 +18,41 @@ class App
     puts ' Please select an option from the list above: '
   end
 
+  # rubocop:disable Metrics
   def start
-    input = 0
     options
-    while input.to_i < 13
-      print ': '
-      input = gets.chomp
-      @app.handle_options(input)
+    print 'Enter Option: '
+    input = gets.chomp
+    case input
+    when '1'
+      puts 'list of books'
+    when '2'
+      puts 'list all music albums'
+    when '3'
+      puts 'List all movies'
+    when '4'
+      puts 'List all games'
+    when '5'
+      puts 'List all genres'
+    when '6'
+      puts 'List all authors'
+    when '7'
+      puts 'List all sources'
+    when '8'
+      puts 'List all labels'
+    when '9'
+      puts 'Add a book'
+    when '10'
+      puts 'Add a music album'
+    when '11'
+      puts 'Add a movie'
+    when '12'
+      puts 'Add a game'
+    when '13'
+      puts 'Bye....'
+    else
+      puts 'Invalid option'
     end
   end
+  # rubocop:enable Metrics
 end
