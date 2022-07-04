3c96abe57cad2cb9edc77497fc843308130c7c24
aboongm <aboongm@yahoo.com>
2022-07-04 15:57:14 +0530

    Add correction for linter

--- lib/item.rb
+++ lib/item.rb
@@ -1,13 +1,12 @@
 class Item
-    def initialize
-        @id
-        @genre
-        @author
-        @source
-        @label
-        @publish_date
-        @archived
-    end
+  attr_accessor :publish_date, :archived, :id
+  attr_reader :genre, :author, :source, :label
+
+  def initialize(publish_date, id = Random.rand(1..5000), archived: false)
+    @id = id
+    @publish_date = publish_date
+    @archived = archived
+  end
 
   def can_be_archived?
     puts 'to be implemented'
