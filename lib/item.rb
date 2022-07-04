2bde68a2694aca07e0c834efc74954b215d42e36
aboongm <aboongm@yahoo.com>
2022-07-04 15:40:55 +0530

    Add setup files, and item.rb and Item class with structure for method

---
+++ lib/item.rb
@@ -0,0 +1,19 @@
+class Item
+    def initialize
+        @id
+        @genre
+        @author
+        @source
+        @label
+        @publish_date
+        @archived
+    end
+
+  def can_be_archived?
+    puts 'to be implemented'
+  end
+
+  def move_to_archive
+    puts 'to be implemented'
+  end
+end
