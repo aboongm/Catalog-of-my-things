dcd86ea3e492f2644b96f2f57a7fe25a2425c15c
sainingo <nickmentor421@gmail.com>
2022-07-05 12:16:38 +0300

    Modify source file

--- lib/source.rb
+++ lib/source.rb
@@ -1,11 +1,10 @@
 class Source
-  attr_accessor :name
-  attr_reader :id, :items
+  attr_accessor :name, :id, :items
 
   def initialize(name)
     @name = name
     @items = []
-    @id = Random.rand(1..500_000)
+    @id = Random.rand(1..1000)
   end
 
   def add_source(item)
