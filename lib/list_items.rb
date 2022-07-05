0cb4554d604f945fd900ed18ad797152beadbbd2
aboongm <aboongm@yahoo.com>
2022-07-05 17:55:54 +0530

    Update create_book.rb and list_items.rb

--- lib/list_items.rb
+++ lib/list_items.rb
@@ -3,7 +3,8 @@ class ListItems
     puts "\nList of Books: "
     puts 'There is no book in the list. Please add a book!' if books.length.zero?
     books.each_with_index do |book, index|
-      puts "#{index + 1}. #{book.title} was published in the year #{book.publish_date} by the publisher #{book.publisher}"
+      puts "#{index + 1}. \t#{book.title} was published in the year #{book.publish_date}
+      \tby the publisher #{book.publisher}"
     end
     puts "\n"
   end
@@ -11,10 +12,9 @@ class ListItems
   def show_labels_list(labels)
     puts "\nList of Labels: "
     puts 'There is no label in the list. Please add a label!' if labels.length.zero?
-    labels.each do |label|     
+    labels.each do |label|
       puts "Title: #{label.title}, Color: #{label.color}"
     end
     puts "\n"
   end
-
 end
