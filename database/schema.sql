eb4fce434574ea9c548c85b5455d130f1bb81a43
aboongm <aboongm@yahoo.com>
2022-07-06 09:22:27 +0530

    Creat a books table  in catalog database

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,14 @@ CREATE TABLE IF NOT EXISTS labels (
   label_title TEXT,
   label_color TEXT
 );
+
+CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  title TEXT,
+  publisher TEXT,
+  cover_state TEXT,
+  publish_date DATE,
+  archived BOOLEAN,
+  label_id INTEGER,
+  FOREIGN KEY (label_id) REFERENCES labels(id)
+);
