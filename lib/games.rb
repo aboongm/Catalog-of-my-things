409f5198f5f55f85b92c001d7d113028dc7aaf9e
misheck12 <misheck1720@gmail.com>
2022-07-06 11:53:10 +0200

    fix author testing failures

--- lib/games.rb
+++ lib/games.rb
@@ -3,17 +3,14 @@ require_relative 'item'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
-
   def initialize(multiplayer, last_played_at, publish_date)
     super(publish_date)
     @multiplayer = multiplayer
-    @last_played_at = Date.parse(last_played_at)
+    @last_played_at = last_played_at
   end
 
   private
-
   def can_be_archived?
-    period = Date.today.year - @last_played_at.year
-    super && period > 2
+    Date.parse(@last_played_at).next_year(2) > Date.today && super
   end
 end
