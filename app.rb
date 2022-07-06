6fe693ea13d94dd9221a5ea8010c1e87c50027d8
Damola Adediran <tylher123@gmail.com>
2022-07-06 08:41:57 +0100

    Implement load and save methods for music data

--- app.rb
+++ app.rb
@@ -1,72 +1,50 @@
 require 'json'
-
 require_relative './lib/list_items'
-
 require_relative './lib/create_book'
-
 require_relative './modules/save_books'
-
 require_relative './modules/load_books'
-
+require_relative './modules/save_music_album'
+require_relative './modules/load_music_album'
 require_relative './lib/label'
+require_relative './lib/music_album'
+require_relative './lib/genre'
 
 class App
   attr_accessor :books, :labels
-
   attr_reader :music_albums, :genres
 
   include SaveBookData
-
   include LoadBookData
+  include LoadMusicData
+  include SaveMusicData
 
   def initialize
     @books = load_books
-
     @labels = load_labels
-
     @list_items = ListItems.new
-
-    @music_albums = []
-
-    @genres = []
+    @music_albums = load_music_albums
+    @genre_names = load_genres_names
+    @genres = load_music_genres
   end
 
   def start
     loop do
-      puts '
-
-    Please choose an option by entering a number:
-
+      puts 'Please choose an option by entering a number:
      1 - List all books
-
      2 - List all music albums
-
      3 - List all movies
-
      4 - List all games
-
      5 - List all genres
-
      6 - List all authors
-
      7 - List all sources
-
      8 - List all labels
-
      9 - Add a book
-
      10 - Add a music album
-
      11 - Add a movie
-
      12 - Add a game
-
      13 - Exit'
-
       puts ' Please select an option from the list above: '
-
       input = gets.chomp
-
       options(input)
     end
   end
@@ -75,112 +53,86 @@ class App
 
   def options(input)
     case input
-
     when '1'
-
       @list_items.show_books_list(@books)
-
     when '2'
-
-      puts 'list all music albums'
-
+      list_music_albums
     when '3'
-
       puts 'List all movies'
-
     when '4'
-
       puts 'List all games'
-
     when '5'
-
-      puts 'List all genres'
-
+      list_genres
     when '6'
-
       puts 'List all authors'
-
     when '7'
-
       puts 'List all sources'
-
     when '8'
-
       @list_items.show_labels_list(@labels)
-
     when '9'
-
       @books << CreateBook.new.create_book
-
     when '10'
-
-      puts 'Add a music album'
-
+      add_music_album(create_music_album)
     when '11'
-
       puts 'Add a movie'
-
     when '12'
-
       puts 'Add a game'
-
     when '13'
-
       save_data(@books)
-
+      save_music_albums(@music_albums)
+      save_music_genres(@genres)
+      save_genre_names(@genre_names)
       puts 'Thank you for using The App, Bye...'
-
       exit
-
     else
-
       puts 'Invalid option'
-
     end
   end
 
   # rubocop:enable Metrics
 
   def create_music_album
-    puts 'when was the album published? [yyyy-mm-dd]: '
-
+    print 'when was the album published? [yyyy-mm-dd]: '
     date_published = gets.chomp
-
     puts 'is album on spotify? [Y/N]: '
-
     ans = gets.chomp.downcase
-
     on_spotify = input_to_boolean(ans)
-
-    @music_album = MusicAlbum.new(date_published, on_spotify: on_spotify)
-
+    puts 'what genre does this album fall under?'
+    genre_name = gets.chomp
+    @music_album = MusicAlbum.new(date_published, false, on_spotify: on_spotify)
+    @music_album.add_genre(Genre.new(genre_name))
+    add_genre(Genre.new(genre_name))
     puts 'Music album created!'
-
     @music_album
+  end
+
+  def list_music_albums
+    @music_albums.each { |a| puts "id: #{a.id}, genre: #{a.genre.name}, date-published: #{a.publish_date}" }
+  end
+
+  def list_genres
+    @genres.each do |g|
+      if g == @genres[@genres.length - 1]
+        puts g.name.to_s
+      else
+        print "#{g.name}, "
+      end
+    end
   end
 
   def create_genre
     puts "Please type name of genre\n"
-
     name = gets.chomp
-
-    if @genres.include?(name)
-
-      puts "#{name} exists already"
-
-    else
-
-      genre = Genre.new(name)
-
-      puts 'genre created successfully'
-
-      genre
-
-    end
+    genre = Genre.new(name)
+    puts 'genre created successfully'
+    genre
   end
 
   def add_genre(genre)
-    @genres << genre.name unless @genres.include?(genre.name)
+    return if @genre_names.include?(genre.name)
+
+    @genres << genre
+    @genre_names << genre.name
   end
 
   def add_music_album(record)
@@ -189,21 +141,14 @@ class App
 
   def input_to_boolean(input)
     case input
-
     when 'y'
-
       true
-
     when 'n'
-
       false
-
     else
-
       puts 'invalid response'
-
-      input_to_boolean(input)
-
+      print 'is album on spotify? [Y/N]: '
+      input_to_boolean(gets.chomp)
     end
   end
 end
--- modules/save_music_album.rb
+++ modules/save_music_album.rb
@@ -1,3 +1,27 @@
-def save_music_albums(albums)
-  File.write('./data/music_album.rb', JSON.generate(albums))
+require 'json'
+module SaveMusicData
+  def save_music_albums(albums)
+    data = []
+    albums.each do |album|
+      data << { id: album.id, genre: album.genre.name,
+                date_published: album.publish_date, archived: album.archived, on_spotify: album.on_spotify }
+    end
+    File.write('./data/music_album.json', JSON.generate(data))
+  end
+
+  def save_music_genres(genres)
+    data = []
+    genres.each do |genre|
+      data << { id: genre.id, name: genre.name }
+    end
+    File.write('./data/genre.json', JSON.generate(data))
+  end
+
+  def save_genre_names(genre_names)
+    data = []
+    genre_names.each do |name|
+      data << name
+    end
+    File.write('./data/genre_names.json', JSON.generate(data))
+  end
 end
