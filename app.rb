12adfd9d13983e066ac3096be751247758007ef4
misheck12 <misheck1720@gmail.com>
2022-07-07 10:24:15 +0200

    fix Conflicts

--- app.rb
+++ app.rb
@@ -10,29 +10,46 @@ require_relative './lib/list_games'
 require_relative './modules/save_games'
 require_relative './modules/save_author'
 require_relative './lib/author'
-
+require_relative './lib/music_album'
+require_relative './lib/genre'
+require_relative './lib/create_music_album'
+require_relative './lib/list_music_data'
+require_relative './modules/save_music_album'
+require_relative './modules/load_music_album'
+require_relative './lib/create_movie'
+require_relative './modules/save_movies'
+require_relative './modules/load_movies'
+require_relative './lib/list_movies'
+require_relative './lib/source'
 class App
-  attr_accessor :books, :labels, :authors, :games
+  attr_accessor :books, :labels, :movies, :source, :authors, :games
   attr_reader :music_albums, :genres
-
   include SaveBookData
   include LoadBookData
   include GameModule
   include SaveGameData
   include SaveAuthorData
   include AuthorModule
-
+  include LoadMusicData
+  include SaveMusicData
+  include SaveMoviesData
+  include LoadMovieData
+  
   def initialize
     @books = load_books
     @labels = load_labels
     @list_items = ListItems.new
-    @music_albums = []
-    @genres = []
+    
+    @music_albums = load_music_albums
+    @genre_names = load_genres_names
+    @genres = load_music_genres
     @games = fetch_games
     @authors = fetch_authors
     @list_games = ListGames.new
+    @movies = load_movies
+    @sources = load_source
+    @list_movies = ListMovies.new
   end
-
   def start
     loop do
       puts "
@@ -48,7 +65,6 @@ class App
     end
   end
   # rubocop:disable Metrics
-
   def options(input)
     case input
     when '1'
@@ -77,11 +93,11 @@ class App
       music_option = gets.chomp
       case music_option
       when '1'
-        puts 'list Music Albums'
+        list_music_albums
       when '2'
-        puts 'list Genres'
+        list_genres
       when '3'
-        puts 'Add music album'
+        add_music_album(@music_albums, create_music_album)
       else
         puts "\tInvalid Option!"
       end
@@ -111,18 +127,20 @@ class App
       movie_option = gets.chomp
       case movie_option
       when '1'
-        puts 'list Movies'
+        @list_movies.show_movie_list(@movies)
       when '2'
-        puts 'list Sources'
+        @list_movies.show_source_list(@sources)
       when '3'
-        puts 'Add Movie'
+        CreateMovie.new.create_movie(@movies, @sources)
       else
         puts "\tInvalid Option!"
       end
     when '5'
       save_data(@books, @labels)
+      save_music_data(@music_albums, @genres, @genre_names)
       save_author(@authors)
       save_games(@games)
+      save_movie_data(@movies, @sources)
       puts "\tThank you for using The App, Bye..."
       exit
     else
@@ -130,47 +148,10 @@ class App
     end
   end
   # rubocop:enable Metrics
-
-  def create_music_album
-    puts 'when was the album published? [yyyy-mm-dd]: '
-    date_published = gets.chomp
-    puts 'is album on spotify? [Y/N]: '
-    ans = gets.chomp.downcase
-    on_spotify = input_to_boolean(ans)
-    @music_album = MusicAlbum.new(date_published, on_spotify: on_spotify)
-    puts 'Music album created!'
-    @music_album
-  end
-
-  def create_genre
-    puts "Please type name of genre\n"
-    name = gets.chomp
-    if @genres.include?(name)
-      puts "#{name} exists already"
-    else
-      genre = Genre.new(name)
-      puts 'genre created successfully'
-      genre
-    end
-  end
-
-  def add_genre(genre)
-    @genres << genre.name unless @genres.include?(genre.name)
-  end
-
-  def add_music_album(record)
-    @music_albums << record
-  end
-
-  def input_to_boolean(input)
-    case input
-    when 'y'
-      true
-    when 'n'
-      false
-    else
-      puts 'invalid response'
-      input_to_boolean(input)
-    end
-  end
 end
+
+
+
+
+
+
